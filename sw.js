846692690d04debb932ab3c95c4679b9be626d9b
Bus Time-BLG <balangodabustime@gmail.com>
2026-02-25 09:27:45 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+﻿const CACHE_NAME = 'Bus Time Finder';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install Event: Cache essential files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
